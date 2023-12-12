24fa9a87eaf1c01aa1e1e70d4b490f86069fe009
SpaceNerd24 <144254541+SpaceNerd24@users.noreply.github.com>
2023-12-12 21:20:24 +0000

    Basic Upgrade System

--- css&js/index.js
+++ css&js/index.js
@@ -1,15 +1,18 @@
 let score = 0;
+let upgrade = 1;
 
 window.onload = function () {
-    score = localStorage.getItem("score") ? parseInt(localStorage.getItem("score")): 0 
+    score = localStorage.getItem("score") ? parseInt(localStorage.getItem("score")): 0;
     document.getElementById("score").innerHTML = (score);
     toohigh();
 }
 function add(){
-        score++;
+        score+=upgrade;
         document.getElementById("score").innerHTML = (score);
         localStorage.setItem("score", score.toString());
         toohigh();
+        work();
+        checker();
 }
 let pin 
 function bypass() {
@@ -18,20 +21,51 @@ function bypass() {
         score = prompt("What do you want your score to be?");
         document.getElementById("score").innerHTML = (score);
         localStorage.setItem("score", score.toString());
-        toohigh()
+        toohigh();
     } else {
         alert("No cheating!");
-        score = 1
+        score = 1;
+        upgrade = 1;
         document.getElementById("score").innerHTML = (score);
-        toohigh()
+        stop2 = 1
+        stop1 = 1
     }
 }
-// anticheat function
+// anticheat functions
 function toohigh() {
-    if (score >= 10000000000000000000000000000) {
+    if (score >= 100000000000000000000000) {
        alert("Cheater!");
        score = 0;
+       upgrade = 1;
        document.getElementById("score").innerHTML = (score);
        localStorage.setItem("score", score.toString());
+    }
+}
+function reset() {
+    score = 1;
+    upgrade = 1;
+    localStorage.setItem("score", score.toString());
+    document.getElementById("score").innerHTML = (score);
+    stop2 = 1
+    stop1 = 1
+}
+// Upgrade Stuff
+function test() {
+    alert(upgrade);
+}
+let stop1 = 1
+let stop2 = 1
+function work() {
+    if (score >= 50) {
+        if (stop1 == 1) {
+            upgrade++;
+            stop1++;
+        }
+    }
+    if (score >= 100) {
+        if (stop2 == 1) {
+            upgrade++;
+            stop2++;
+        }
     }
 }

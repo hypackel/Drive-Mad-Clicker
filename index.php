a3d55a874f0b32c292701f11d98f4923c4c8d868
SpaceNerd24 <144254541+SpaceNerd24@users.noreply.github.com>
2024-01-03 15:05:49 +0000

    dsklfh

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="0; url=/home.php">
+    <title>Home | Hypackel</title>
+</head>
+<body>
+    <div align="center" >
+        <h1>Hello</h1>
+    </div>
+    <script>
+        window.location.replace("/home.php");
+    </script>
+</body>
+</html>
